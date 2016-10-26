baf681f97cfff802449d027db34828b41126c485
oleg <oleynikoleg1983@gmail.com>
2016-10-26 16:21:15 +0300

    Bug #15688 Refactoring to graph "Overview success"

--- src/iceberg-chart.js
+++ src/iceberg-chart.js
@@ -20,17 +20,21 @@
         var labels = options.labels || {
                 'success': 'Success',
                 'fail': 'Fail',
+                'inprogress': 'Inprogress',
                 'unknown': 'Unknown'
             };
         var labelColors = options.labelColors || {
                 'success': '#2CBD6E',
                 'fail': '#FF5151',
+                'inprogress': '#FF5151',
                 'unknown': '#FF5151'
             };
         var colors = options.labelColors || {
-                'background': '#9CC3E5',
+                'background': '#83b4e0',
                 'waterLine': '#2E75B5',
-                'unknownPartBg': '#BDD7EE',
+                'inprogressPartBg': '#cbdff1',
+                'inprogressPart': '#FFFFFF',
+                'unknownPartBg': '#99c0e3',
                 'unknownPart': '#FFFFFF',
                 'overWaterPartBg': '#FFFFFF',
                 'overWaterPart': '#F3F6FB'
@@ -52,6 +56,7 @@
             var icebergWidth = this.canvas.width - vOffset * 2;
             var bottomHeight = Math.ceil(icebergHeight * this.data.unknown / 100);
             var topHeight = Math.ceil(icebergHeight * (this.data.success + this.data.fail) / 100);
+            var inprogressHeight = Math.ceil(icebergHeight * (this.data.inprogress) / 100); //add inprogress
             var successHeight = Math.ceil(icebergHeight * this.data.success / 100);
 
             //console.log('xCenter', xCenter);
@@ -62,13 +67,15 @@
             //console.log('bottomHeight', bottomHeight);
             //console.log('topHeight', topHeight);
 
-
             this.drawBg(hOffset + successHeight + 1);
 
-            var points = this.drawWhiteIceberg(xCenter, hOffset, vOffset, topHeight, icebergHeight, icebergWidth);
-            //console.log(points);
-            this.drawUnknownPart(xCenter, hOffset + topHeight + 1, points);
+            var points = this.drawWhiteIceberg(xCenter, hOffset, vOffset, topHeight, icebergHeight, icebergWidth, icebergHeight - bottomHeight);
 
+            if (this.data.inprogress > 0) {
+                this.drawInprogressPart(xCenter, hOffset + topHeight + 1, points);
+            }
+
+            this.drawUnknownPart(xCenter, hOffset + topHeight + 1, points, icebergHeight - bottomHeight);
             this.drawWaterLine(hOffset + successHeight);
 
             if (this.data.success > 0) {
@@ -82,10 +89,20 @@
                     'right', labels.fail + ' - ' + this.data.fail + '%', labelColors.fail);
             }
 
+            if (this.data.inprogress > 0) {
+                this.drawLabel(xCenter, hOffset + inprogressHeight + topHeight + 10 - Math.ceil((icebergHeight * this.data.inprogress / 100) / 2) - 14,
+                    hOffset,
+                    hOffset + topHeight + 15,
+                    'left',
+                    labels.inprogress + ' - ' + this.data.inprogress + '%', labelColors.inprogress);
+            }
+
             if (this.data.unknown > 0) {
-                this.drawLabel(xCenter, icebergHeight - Math.ceil(bottomHeight / 4),
-                    this.canvas.width + hOffset / 2 - 10, icebergHeight - Math.ceil(bottomHeight / 4) - 10,
-                    'right', labels.unknown + ' - ' + this.data.unknown + '%', labelColors.unknown);
+                this.drawLabel(xCenter, icebergHeight - Math.ceil(bottomHeight / 4) + 7,
+                    this.canvas.width + hOffset / 2 - 10,
+                    hOffset + topHeight + 15 + inprogressHeight, //icebergHeight - Math.ceil((bottomHeight +inprogressHeight+faiHeight)/ 4)+14,
+                    'right', labels.unknown + ' - ' + this.data.unknown + '%',
+                    labelColors.unknown);
             }
         }
 
@@ -108,7 +125,7 @@
             this.ctx.restore();
         }
 
-        this.drawWhiteIceberg = function (xCenter, hOffset, vOffset, topHeight, icebergHeight, icebergWidth) {
+        this.drawWhiteIceberg = function (xCenter, hOffset, vOffset, topHeight, icebergHeight, icebergWidth, unknownHeight) {
             var points = [];
             this.ctx.lineWidth = 1;
             this.ctx.miterLimit = 10;
@@ -117,11 +134,12 @@
             //this.ctx.strokeStyle = '#000000';
             this.ctx.beginPath();
 
+
             this.ctx.moveTo(xCenter, hOffset);
-            this.drawBrokenLine(xCenter, hOffset, vOffset, hOffset + topHeight, -1);
-            points = points.concat(this.drawBrokenLine(vOffset, hOffset + topHeight, xCenter, hOffset + icebergHeight, 1));
-            points = points.concat(this.drawBrokenLine(xCenter, hOffset + icebergHeight, vOffset + icebergWidth, hOffset + topHeight, 1));
-            this.drawBrokenLine(vOffset + icebergWidth, hOffset + topHeight, xCenter, hOffset, -1);
+            this.drawBrokenLine(xCenter, hOffset, vOffset, hOffset + topHeight, -1, null, null);
+            points = points.concat(this.drawBrokenLine(vOffset, hOffset + topHeight, xCenter, hOffset + icebergHeight, 1, unknownHeight, 'left'));
+            points = points.concat(this.drawBrokenLine(xCenter, hOffset + icebergHeight, vOffset + icebergWidth, hOffset + topHeight, 1, unknownHeight, 'right'));
+            this.drawBrokenLine(vOffset + icebergWidth, hOffset + topHeight, xCenter, hOffset, -1, null, null);
 
             this.ctx.closePath();
             this.ctx.fill();
@@ -129,11 +147,11 @@
             return points;
         }
 
-        this.drawUnknownPart = function (x, y, points) {
+        this.drawInprogressPart = function (x, y, points, color) {
             this.ctx.lineWidth = 1;
             this.ctx.miterLimit = 10;
-            this.ctx.fillStyle = colors.unknownPartBg;
-            this.ctx.strokeStyle = colors.unknownPart;
+            this.ctx.fillStyle = colors.inprogressPartBg;
+            this.ctx.strokeStyle = colors.inprogressPart;
             this.ctx.beginPath();
             this.ctx.moveTo(x, y);
             var self = this;
@@ -143,6 +161,25 @@
             this.ctx.closePath();
             this.ctx.fill();
             this.ctx.stroke();
+        }
+
+
+        this.drawUnknownPart = function (x, y, points, unknownHeight) {
+            this.ctx.lineWidth = 1;
+            this.ctx.miterLimit = 10;
+            this.ctx.fillStyle = colors.unknownPartBg;
+            this.ctx.strokeStyle = colors.unknownPart;
+            this.ctx.beginPath();
+            var self = this;
+            points.forEach(function (point, index) {
+                if (point[1] > (unknownHeight + 13)) {
+                    self.ctx.lineTo(point[0] + 1, point[1] + 1);
+                }
+            });
+            this.ctx.closePath();
+            this.ctx.fill();
+            this.ctx.stroke();
+
         }
 
         this.drawBg = function (y) {
@@ -222,26 +259,66 @@
             return parts;
         }
 
-        this.drawBrokenLine = function (x1, y1, x2, y2, direction) {
+        this.drawBrokenLine = function (x1, y1, x2, y2, direction, unknownHeight, position) {
             var points = [];
 
             var parts = this.getRandomParts(x1, y1, x2, y2);
             var self = this;
+            unknownHeight = (unknownHeight) ? (unknownHeight + 14) : false;
             points.push([
                 x1, y1
             ]);
-            parts.forEach(function (part) {
+            var setUnknownHeightY1 = false;
+            var setUnknownHeightY2 = false;
+
+            parts.forEach(function (part, i) {
+
+
                 var subLineLength = self.getRandomInt(2, 8);
                 var y1Offset = self.getRandomInt(-3, 3);
                 var y2Offset = self.getRandomInt(-3, 3);
+                var drowY1 = part[1] + y1Offset;
+                var drowY2 = part[1] + y2Offset;
 
-                self.ctx.lineTo(part[0] - direction * subLineLength, part[1] + y1Offset);
-                self.ctx.lineTo(part[0] + direction * subLineLength, part[1] + y2Offset);
+                if (unknownHeight && !setUnknownHeightY1 && unknownHeight <= drowY1) {
+                    drowY1 = unknownHeight;
+                    setUnknownHeightY1 = true;
+                }
+
+                if (unknownHeight && !setUnknownHeightY2 && unknownHeight <= drowY2) {
+                    drowY2 = unknownHeight;
+                    setUnknownHeightY2 = true;
+                }
+
+                if (setUnknownHeightY1 && drowY1 >= unknownHeight) {
+                    drowY1 = unknownHeight;
+                }
+
+                if (setUnknownHeightY2 && drowY2 >= unknownHeight) {
+                    drowY2 = unknownHeight;
+                }
+
+                if (position == 'left' && unknownHeight && i == (parts.length - 1) && !setUnknownHeightY1 && !setUnknownHeightY2) {
+                    drowY1 = unknownHeight;
+                    drowY2 = unknownHeight;
+                    setUnknownHeightY2 = true;
+                    setUnknownHeightY1 = true;
+                }
+
+                if (position == 'right' && !setUnknownHeightY2 && !setUnknownHeightY1 && unknownHeight) {
+                    drowY1 = unknownHeight;
+                    drowY2 = unknownHeight;
+                    setUnknownHeightY1 = true;
+                    setUnknownHeightY2 = true;
+                }
+
+                self.ctx.lineTo(part[0] - direction * subLineLength, drowY1);
+                self.ctx.lineTo(part[0] + direction * subLineLength, drowY2);
                 points.push([
-                    part[0] - direction * subLineLength, part[1] + y1Offset
+                    part[0] - direction * subLineLength, drowY1, unknownHeight
                 ]);
                 points.push([
-                    part[0] + direction * subLineLength, part[1] + y2Offset
+                    part[0] + direction * subLineLength, drowY2, unknownHeight
                 ]);
             });
 
@@ -249,7 +326,6 @@
                 x2, y2
             ]);
             self.ctx.lineTo(x2, y2);
-
             return points;
         }
     }
@@ -257,4 +333,5 @@
     exports.IcebergChart = IcebergChart;
     window.IcebergChart = IcebergChart;
     return IcebergChart;
+
 }));
